5f54664ecbdf6f5141a3d4943df667a5da5c13e9
Egor Rokin <rokinea89mone@mail.ru>
2019-02-01 19:06:46 +0500

    Updates

---
+++ precache-manifest.e933704d9642f1c08ea17e4f79394075.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1e9c45d8810568716ec6",
+    "url": "/static/js/main.1e9c45d8.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "1e9c45d8810568716ec6",
+    "url": "/static/css/main.dd2aa343.chunk.css"
+  },
+  {
+    "revision": "3790d2d7ec1a6ee768cdb4aa84a9a86b",
+    "url": "/index.html"
+  }
+];
